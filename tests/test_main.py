98d66b41e56a3c6b2bd97e847b9706c29491be61
Christiaan Meijer <c.meijer@esciencecenter.nl>
2024-10-09 16:23:51 +0200

    add integration test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,17 @@ from pathlib import Path
 
 import pytest
 
-from main import get_features_path, FeatureType
+from segmentmytiff.main import read_input_and_labels_and_save_predictions, FeatureType, get_features_path
 
+
+def test_integration():
+    input_path = Path("test_data") / "test_image.tif"
+    labels_path = Path("test_data") / "test_image_labels.tif"
+    predictions_path = Path("test_data") / "test_image_predictions.tif"
+
+    read_input_and_labels_and_save_predictions(input_path, labels_path, predictions_path)
+
+    assert predictions_path.exists()
 
 @pytest.mark.parametrize("input_path, feature_type, expected_path", [
     ("input.tiff", FeatureType.FLAIR, "input_FLAIR.tiff"),
