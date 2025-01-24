dd52db06ab8f65385a653c385e62787a65352b39
Ou Ku <o.ku@esciencecenter.nl>
2025-01-24 16:48:16 +0100

    draft update on read_input_and_labels_and_save_predictions, need to further work on feature extractor

--- src/segmentmytif/features.py
+++ src/segmentmytif/features.py
@@ -2,6 +2,8 @@ import logging
 from enum import Enum
 from pathlib import Path
 
+from dask.array.core import Array
+import xarray as xr
 import numpy as np
 import torch
 from numpy import ndarray
@@ -25,30 +27,32 @@ class FeatureType(Enum):
             raise ValueError()
 
 
-def get_features(input_data: np.ndarray, input_path: Path, feature_type: FeatureType, features_path: Path, profile,
-                 **extractor_kwargs):
+def get_features(
+    raster: xr.DataArray, input_path: Path, feature_type: FeatureType, features_path: Path, **extractor_kwargs
+):
     """
     Extract features from the input data, or load them from disk if they have already been extracted.
     :param input_data: 'Raw' input data as stored in TIFs by a GIS user. Shape: [n_bands, height, width]
     :param input_path:
     :param feature_type: See FeatureType enum for options.
     :param features_path: Path used for caching features
-    :param profile:
     :param extractor_kwargs: options for the feature extractor
     :return:
     """
     if feature_type == FeatureType.IDENTITY:
-        return input_data
+        return raster
     else:
         if features_path is None:
             features_path = get_features_path(input_path, feature_type)
         if not features_path.exists():
-            logger.info(f"No existing {feature_type.name} features found at {features_path} for input data with shape {input_data.shape}")
+            logger.info(
+                f"No existing {feature_type.name} features found at {features_path} for input data with shape {raster.data.shape}"
+            )
             with log_duration(f"Extracting {feature_type.name} features", logger):
-                features = extract_features(input_data, feature_type, **extractor_kwargs)
-            log_array(features, logger, array_name=f"{feature_type.name} features")
-            logger.info(f"Saving {feature_type.name} features (shape {features.shape}) to {features_path}")
-            save_tiff(features, features_path, profile)
+                features_data = extract_features(raster.data, feature_type, **extractor_kwargs)
+            log_array(features_data, logger, array_name=f"{feature_type.name} features")
+            logger.info(f"Saving {feature_type.name} features (shape {features_data.shape}) to {features_path}")
+            features = xr.DataArray(features_data, dims=raster.dims).compute()
         loaded_features, _ = read_geotiff(features_path)
         logger.info(f"Loading {feature_type.name} features (shape {loaded_features.shape}) from {features_path}")
         return loaded_features
@@ -60,7 +64,10 @@ def extract_features(input_data, feature_type, **extractor_kwargs):
         FeatureType.FLAIR: extract_flair_features,
     }[feature_type]
 
-    return extractor(input_data, **extractor_kwargs)
+    if isinstance(input_data, Array):  # If dask array, map feature extraction function to each block
+        return input_data.map_blocks(extractor, **extractor_kwargs)
+    else:  # else, assume numpy array
+        return extractor(input_data, **extractor_kwargs)
 
 
 def extract_identity_features(input_data: ndarray) -> ndarray:
@@ -80,25 +87,25 @@ def extract_flair_features(input_data: ndarray, model_scale=1.0) -> ndarray:
 
     outputs = []
     for i_band in range(n_bands):
-        input_band = torch.from_numpy(input_data[None, i_band:i_band + 1, :, :]).float().to(device)
+        input_band = torch.from_numpy(input_data[None, i_band : i_band + 1, :, :]).float().to(device)
         padded_input = pad(input_band, band_name=i_band)
         padded_current_predictions = model(padded_input).detach().numpy()
-        current_predictions = padded_current_predictions[:, :, :input_band.shape[2], :input_band.shape[3]]  #unpad
+        current_predictions = padded_current_predictions[:, :, : input_band.shape[2], : input_band.shape[3]]  # unpad
         outputs.append(current_predictions)
     output = np.concatenate(outputs, axis=1)
     return output[0, :, :, :]
 
 
-def load_model(model_scale:float):
+def load_model(model_scale: float):
     """
     Load the model from disk and return it along with the device it's loaded on to.
     :param model_scale: Scale of the model to use. Must be one of [1.0, 0.5, 0.25, 0.125]
     :return: Torch model and the device it's loaded on to
     """
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = UNet(in_channels=1, num_classes=NUM_FLAIR_CLASSES, model_scale=model_scale)
     file_name = get_flair_model_file_name(model_scale)
-    state = torch.load(Path("models") / file_name, map_location=device, weights_only=True)
+    state = torch.load(Path(__file__).parent / "../../models" / file_name, map_location=device, weights_only=True)
     model.load_state_dict(state)
     model.eval()
     return model, device
@@ -118,19 +125,16 @@ def pad(input_band, band_name):
         pad_width = 16 - width % 16
         pad_height = 16 - height % 16
         padded = torch.nn.functional.pad(input_band, (0, pad_height, 0, pad_width))
-        logger.info(f"Added temporary padding for band {band_name}: (original {height} x {width})"
-                    f" -> (padded {height + pad_height} x {width + pad_width})")
+        logger.info(
+            f"Added temporary padding for band {band_name}: (original {height} x {width})"
+            f" -> (padded {height + pad_height} x {width + pad_width})"
+        )
 
     return padded
 
 
 def get_flair_model_file_name(model_scale: float) -> str:
-    scale_mapping = {
-        1.0: "1_0",
-        0.5: "0_5",
-        0.25: "0_25",
-        0.125: "0_125"
-    }
+    scale_mapping = {1.0: "1_0", 0.5: "0_5", 0.25: "0_25", 0.125: "0_125"}
 
     scale = None
     for k, v in scale_mapping.items():
--- src/segmentmytif/main.py
+++ src/segmentmytif/main.py
@@ -1,32 +1,66 @@
 import argparse
 from pathlib import Path
+from typing import Literal, Optional
 
 import numpy as np
 import dask.array as da
 from numpy import ndarray
 from sklearn.ensemble import RandomForestClassifier
+import geopandas as gpd
+import rioxarray
+import dask
 
 from segmentmytif.features import get_features, FeatureType
 from segmentmytif.logging_config import setup_logger, log_duration, log_array
 from segmentmytif.utils.io import read_geotiff, save_tiff
+from segmentmytif.utils.geospatial import get_label_array
 
 logger = setup_logger(__name__)
 
 
-def read_input_and_labels_and_save_predictions(input_path: Path, labels_path: Path, output_path: Path,
-                                               feature_type=FeatureType.IDENTITY, features_path: Path = None,
-                                               **extractor_kwargs) -> None:
+def read_input_and_labels_and_save_predictions(
+    raster_path: Path,
+    pos_labels_path: Path,
+    neg_labels_path: Path,
+    output_path: Path,
+    feature_type=FeatureType.IDENTITY,
+    features_path: Path = None,
+    mode: Literal["normal", "parallel", "safe"] = "normal",
+    chunks: dict = None,
+    **extractor_kwargs,
+) -> None:
     logger.info("read_input_and_labels_and_save_predictions called with the following arguments:")
     for k, v in locals().items():
         logger.info(f"{k}: {v}")
 
-    input_data, profile = read_geotiff(input_path)
+    if chunks is None:
+        chunks = {"band": 1, "x": 1000, "y": 1000}
 
-    features = get_features(input_data, input_path, feature_type, features_path, profile, **extractor_kwargs)
+    match mode:
+        case "normal":
+            raster = rioxarray.open_rasterio(raster_path)  # Load in memory
+        case "parallel":
+            raster = rioxarray.open_rasterio(raster_path, chunks=chunks)
+        case "safe":
+            dask.config.set(scheduler="synchronous")
+            raster = rioxarray.open_rasterio(raster_path, chunks=chunks)
+        case _:
+            msg = f"Invalid mode: {mode}"
+            raise ValueError(msg)
 
-    labels, _ = read_geotiff(labels_path)
-    prediction_map = make_predictions(features, labels)
+    features = get_features(raster, raster_path, feature_type, features_path, **extractor_kwargs)
 
+    # Load vector labels as geodataframes, and align CRS with input data
+    pos_gdf = gpd.read_file(pos_labels_path).to_crs(raster.rio.crs)
+    neg_gdf = gpd.read_file(pos_labels_path).to_crs(raster.rio.crs)
+
+    # Get label arrays
+    labels = get_label_array(raster, pos_gdf, neg_gdf, mode=mode)
+
+    # Make predictions
+    prediction_map = make_predictions(features.data, labels)
+
+    # Save predictions
     save_tiff(prediction_map, output_path, profile)
 
 
@@ -56,24 +90,24 @@ def make_predictions(input_data: ndarray, labels: ndarray) -> ndarray:
 
 def prepare_training_data(input_data, labels):
     """
-        Prepares training data for a binary classification task.
+    Prepares training data for a binary classification task.
 
-        Parameters:
-        - input_data: A 3D array-like object (e.g., NumPy or Dask array) where the first dimension represents
-          instances, and the last two dimensions represent spatial data.
-        - labels: A 3D array-like object (e.g., NumPy or Dask array) where the first dimension is the class index
-          (only single-class supported), and the last two dimensions represent spatial labels.
+    Parameters:
+    - input_data: A 3D array-like object (e.g., NumPy or Dask array) where the first dimension represents
+      instances, and the last two dimensions represent spatial data.
+    - labels: A 3D array-like object (e.g., NumPy or Dask array) where the first dimension is the class index
+      (only single-class supported), and the last two dimensions represent spatial labels.
 
-        Process:
-        1. Flattens the label array for the first class to a 1D array.
-        2. Separates the positive and negative instances from the input data based on the labels.
-        3. Computes the number of labeled and unlabeled instances and logs the statistics.
-        4. Concatenates positive and negative instances into training data and corresponding labels.
+    Process:
+    1. Flattens the label array for the first class to a 1D array.
+    2. Separates the positive and negative instances from the input data based on the labels.
+    3. Computes the number of labeled and unlabeled instances and logs the statistics.
+    4. Concatenates positive and negative instances into training data and corresponding labels.
 
-        Returns:
-        - train_data: A 2D array where each row is a training instance and each column is a feature.
-        - train_labels: A 1D array containing the labels corresponding to the rows in `train_data`.
-        """
+    Returns:
+    - train_data: A 2D array where each row is a training instance and each column is a feature.
+    - train_labels: A 1D array containing the labels corresponding to the rows in `train_data`.
+    """
     # Reshape input data to [n_instances, n_features]
     class1_labels = labels[0]  # Only single class is supported
     flattened = class1_labels.flatten()
@@ -82,7 +116,8 @@ def prepare_training_data(input_data, labels):
     n_labeled = flattened.shape[0]
     n_unlabeled = np.prod(labels.shape[-2:]) - n_labeled
     logger.info(
-        f"Dataset contains {n_labeled} ({round(100 * n_labeled / (n_labeled + n_unlabeled), 2)}%) labeled instances of a total of {n_labeled + n_unlabeled} instances.")
+        f"Dataset contains {n_labeled} ({round(100 * n_labeled / (n_labeled + n_unlabeled), 2)}%) labeled instances of a total of {n_labeled + n_unlabeled} instances."
+    )
     # Subsample training data
     sampled_positive_instances = subsample(positive_instances, 10000)
     sampled_negative_instances = subsample(negative_instances, 10000)
@@ -90,14 +125,19 @@ def prepare_training_data(input_data, labels):
     n_sampled_negative = sampled_negative_instances.shape[0]
     n_sampled_labeled = n_sampled_negative + n_sampled_positive
     logger.info(
-        f"Training on {n_sampled_positive} ({round(100 * n_sampled_positive / n_sampled_labeled, 2)}%) positive labels and {n_sampled_negative} ({round(100 * n_sampled_negative / n_sampled_labeled, 2)}%) negative labels ")
+        f"Training on {n_sampled_positive} ({round(100 * n_sampled_positive / n_sampled_labeled, 2)}%) positive labels and {n_sampled_negative} ({round(100 * n_sampled_negative / n_sampled_labeled, 2)}%) negative labels "
+    )
     # Shuffle training data
     total_sample_size = sampled_positive_instances.shape[0] + sampled_negative_instances.shape[0]
     order = np.arange(total_sample_size)
     np.random.shuffle(order)
     train_data = np.concatenate((sampled_positive_instances, sampled_negative_instances))[order]
-    train_labels = np.concatenate(((np.ones(shape=[sampled_positive_instances.shape[0]])), (np.zeros(shape=[sampled_negative_instances.shape[0]]))))[
-        order]
+    train_labels = np.concatenate(
+        (
+            (np.ones(shape=[sampled_positive_instances.shape[0]])),
+            (np.zeros(shape=[sampled_negative_instances.shape[0]])),
+        )
+    )[order]
     log_array(train_labels, logger, array_name="Train labels")
     log_array(train_data, logger, array_name="Train data")
 
@@ -116,12 +156,17 @@ def subsample(instances, sample_size):
 def parse_args():
     parser = argparse.ArgumentParser(description="Process input and output TIFF files.")
 
-    parser.add_argument('-i', '--input', type=Path, help='Path to the input TIFF file')
-    parser.add_argument('-l', '--labels', type=Path, help='Path to the training labels TIFF file')
-    parser.add_argument('-p', '--predictions', type=Path, help='Path to the predictions output TIFF file')
-    parser.add_argument('-f', '--feature_type', type=FeatureType.from_string, choices=list(FeatureType),
-                        default=FeatureType.IDENTITY,
-                        help='Type of feature being used. "Identity" means the raw input is directly used as features.')
+    parser.add_argument("-i", "--input", type=Path, help="Path to the input TIFF file")
+    parser.add_argument("-l", "--labels", type=Path, help="Path to the training labels TIFF file")
+    parser.add_argument("-p", "--predictions", type=Path, help="Path to the predictions output TIFF file")
+    parser.add_argument(
+        "-f",
+        "--feature_type",
+        type=FeatureType.from_string,
+        choices=list(FeatureType),
+        default=FeatureType.IDENTITY,
+        help='Type of feature being used. "Identity" means the raw input is directly used as features.',
+    )
 
     args = parser.parse_args()
 
@@ -132,7 +177,7 @@ def parse_args():
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     input_path = args.input
     labels_path = args.labels
