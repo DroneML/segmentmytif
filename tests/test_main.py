e04d10ab6cdf2a2b50a410c083d22096fc72939f
Christiaan Meijer <c.meijer@esciencecenter.nl>
2025-02-06 16:27:58 +0100

    fix merge issues in testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,57 +7,41 @@ import pytest
 import rasterio
 import rioxarray
 from scipy.spatial.distance import dice
-import itertools
 
 from segmentmytif.features import FeatureType, get_features_path
 from segmentmytif.main import read_input_and_labels_and_save_predictions, prepare_training_data
 from segmentmytif.utils.io import save_tiff
+from .test_cases import TestCase, test_case1210, test_case512
 from .utils import TEST_DATA_FOLDER
 
-@pytest.mark.parametrize("test_image, test_labels, feature_type, model_scale, dice_similarity_threshold",
+
+@pytest.mark.parametrize("test_case, feature_type, model_scale, dice_similarity_threshold, compute_mode",
                          [
-                             pytest.param("test_image.tif", "test_image_labels.tif", FeatureType.IDENTITY, None, None, marks=pytest.mark.slow),
-                             pytest.param("test_image.tif", "test_image_labels.tif", FeatureType.FLAIR, 0.125, None), # also slow, but necessary to test on each run
-                             pytest.param("test_image_512x512.tif", "test_image_labels_512x512.tif", FeatureType.IDENTITY,None, 0.90, marks=pytest.mark.slow),
-                             pytest.param("test_image_512x512.tif", "test_image_labels_512x512.tif", FeatureType.FLAIR,0.125,  0.84, marks=pytest.mark.slow),
-                             pytest.param("test_image_512x512.tif", "test_image_labels_512x512.tif",  FeatureType.FLAIR,1.0, 0.98, marks=pytest.mark.slow),
+                             pytest.param(test_case1210, FeatureType.IDENTITY, None, None, "normal", marks=pytest.mark.slow),
+                             pytest.param(test_case1210, FeatureType.FLAIR, 0.125, None, "normal"), # also slow, but necessary to test on each run
+                             pytest.param(test_case512, FeatureType.IDENTITY, None, 0.90, "normal", marks=pytest.mark.slow),
+                             pytest.param(test_case512, FeatureType.FLAIR, 0.125, 0.84, "normal", marks=pytest.mark.slow),
+                             pytest.param(test_case512, FeatureType.FLAIR, 1.0, 0.98, "normal", marks=pytest.mark.slow),
+                             pytest.param(test_case1210, FeatureType.IDENTITY, None, None, "parallel", marks=pytest.mark.slow),
+                             pytest.param(test_case1210, FeatureType.FLAIR, 0.125, None, "parallel", marks=pytest.mark.slow),
+                             pytest.param(test_case1210, FeatureType.IDENTITY, None, None, "safe", marks=pytest.mark.slow),
+                             pytest.param(test_case1210, FeatureType.FLAIR, 0.125, None, "safe", marks=pytest.mark.slow),
+
                          ])
-def test_integration(tmpdir, test_image, test_labels, feature_type, model_scale, dice_similarity_threshold):
-    input_path = copy_file_and_get_new_path(test_image, tmpdir)
-    labels_path = copy_file_and_get_new_path(test_labels, tmpdir)
-    predictions_path = Path(tmpdir) / f"{test_image}_predictions_{str(feature_type)}.tif"
-# Test integration with a cmbination of inputs
-test_images_and_labels = [
-    ("test_image.tif", "test_image_labels_positive.gpkg", "test_image_labels_negative.gpkg"),
-    ("test_image_512x512.tif", "test_image_labels_positive_512x512.gpkg", "test_image_labels_negative_512x512.gpkg"),
-]
-feature_types = [FeatureType.IDENTITY, FeatureType.FLAIR]
-compute_modes = ["normal", "parallel", "safe"]
-combinations = list(itertools.product(test_images_and_labels, feature_types, compute_modes))
-
-
-    read_input_and_labels_and_save_predictions(input_path, labels_path,
-                                               predictions_path,
-                                               feature_type=feature_type,
-                                               model_scale=model_scale)
-@pytest.mark.parametrize(
-    ("test_images_and_labels", "feature_type", "compute_modes"),
-    combinations,
-)
-def test_integration(tmpdir, test_images_and_labels, feature_type, compute_modes):
-    input_path = copy_file_and_get_new_path(test_images_and_labels[0], tmpdir)
-    labels_pos_path = copy_file_and_get_new_path(test_images_and_labels[1], tmpdir)
-    labels_neg_path = copy_file_and_get_new_path(test_images_and_labels[2], tmpdir)
-    predictions_path = Path(tmpdir) / f"{test_images_and_labels[0]}_predictions_{str(feature_type)}.tif"
+def test_integration(tmpdir, test_case: TestCase, feature_type, model_scale, dice_similarity_threshold, compute_mode):
+    input_path = copy_file_and_get_new_path(test_case.image_filename, tmpdir)
+    labels_pos_path = copy_file_and_get_new_path(test_case.labels_pos_filename, tmpdir)
+    labels_neg_path = copy_file_and_get_new_path(test_case.labels_neg_filename, tmpdir)
+    predictions_path = Path(tmpdir) / f"{test_case.image_filename}_predictions_{str(feature_type)}.tif"
 
     read_input_and_labels_and_save_predictions(
-        input_path, labels_pos_path, labels_neg_path, predictions_path, feature_type=feature_type, model_scale=0.125
+        input_path, labels_pos_path, labels_neg_path, predictions_path, feature_type=feature_type, model_scale=model_scale
     )
 
     assert predictions_path.exists()
 
     # Check DICE similarity if threshold is provided
-    if dice_similarity_threshold is None:
+    if dice_similarity_threshold is None and test_case.ground_truth_filename is None:
         return
 
     truth = rioxarray.open_rasterio(TEST_DATA_FOLDER / "test_image_512x512_out_ground_truth.tif").astype(np.int16)
